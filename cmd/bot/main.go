729bb69f1055d15224f3c337f88c254ad2f728e3
Prokopov Aleksey <prokopov.vrn@gmail.com>
2023-03-28 17:39:19 +0300

    add new go.mod

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -9,33 +9,57 @@ import (
 
 func main() {
 	godotenv.Load()
-	t:=os.Getenv("TOKEN")
+	t := os.Getenv("TOKEN")
+	productService := product
 
-	bot, err:=tgbotapi.NewBotAPI(t)
-	if err!=nil{
+	bot, err := tgbotapi.NewBotAPI(t)
+	if err != nil {
 		log.Panic(err)
 	}
 	bot.Debug = true
 
 	log.Printf("Authorized on account: %s", bot.Self.UserName)
 
-	u:=tgbotapi.UpdateConfig{
+	u := tgbotapi.UpdateConfig{
 		Timeout: 60,
 	}
 
-	updates, err:= bot.GetUpdatesChan(u)
-	if err!=nil{
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
 		log.Panic(err)
 	}
 
-	for update := range updates{
-		if update.Message == nil{
+	for update := range updates {
+		if update.Message == nil {
 			continue
 		}
+
+		switch update.Message.Command() {
+		case "help":
+			helpCmd(bot, update.Message)
+		case "list":
+			listCmd()
+		default:
+			defCmd()
+		}
+
 		log.Printf("[FROM: %s],TEXT: %s", update.Message.From.UserName, update.Message.Text)
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "echo: " + update.Message.Text)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "echo: "+update.Message.Text)
 		bot.Send(msg)
 	}
+
+}
+
+func helpCmd(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	text := "/help - help\n" +
+		"/list - list products"
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, text)
+	bot.Send(msg)
+}
+
+func listCmd(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, productService *.Service){
+	text:= "all products: \n"
+	products := productService
 
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
-module study256
+module github.com/alekseyprokopov/bot256
 
 go 1.20
 
 require (
-	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/joho/godotenv v1.5.1
 )
+
+require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
